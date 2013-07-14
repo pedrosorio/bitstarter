a62ad783c73ede8d0b14bc80d998a7bded3dfe4e
Pedro Osorio <mebm.pedrosorio@gmail.com>
2013-07-14 21:09:05 +0000

    Modified grader.js to process Urls

--- grader.js
+++ grader.js
@@ -24,6 +24,8 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+var util = require('util');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -36,16 +38,12 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
-};
-
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlString = function(htmlstring, checksfile) {
+    $ = cheerio.load(htmlstring);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -61,14 +59,37 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+
+var gradermain = function(htmlstring, checksfile) {
+    var checkJson = checkHtmlString(htmlstring, checksfile);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+}
+
+var buildfn = function(checksfile) {
+    var response2console = function(result, response) {
+        if (result instanceof Error) {
+            console.error('Error: ' + util.format(response.message));
+        } else {
+	    gradermain(result, checksfile);
+        }
+    };
+    return response2console;
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <html_url>', 'Url to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (program.url) {
+	response2console = buildfn(program.checks);
+	rest.get(program.url).on('complete',  response2console);
+    }
+    else {
+	gradermain(fs.readFileSync(program.file), program.checks);
+    }
 } else {
-    exports.checkHtmlFile = checkHtmlFile;
+    exports.checkHtmlString = checkHtmlString;
 }
